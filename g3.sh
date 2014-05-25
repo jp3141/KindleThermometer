1dbd2612c5b3893fa15bf043b51b49f8ea258063
jp3141 <KindleThermometer>
2014-05-25 14:15:11 -0700

    Update

--- g3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for R in `seq 0 1 799`; do
-cat l2 >> l3
-done
-
-tail -n 800 /var/tmp/thermometer.log|awk -v min=$1 -v max=$2 '{printf("echo -ne \"\xFF\xFF\xFF\xFF\xFF\"|dd bs=1 seek=%i of=/dev/fb0 2> /dev/null\n",(FNR-1)*600+100*($7-min)/(max-min))}'|sh
-
